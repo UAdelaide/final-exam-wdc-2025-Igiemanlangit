382c19a86e8e9e8dda0221bd65ddc75d5ffcca19
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-20 21:55:06 +0000

    [automatic commit] 2025-06-20 21:55:06 UTC 1.121.178.119

--- part2/app.js
+++ part2/app.js
@@ -43,15 +43,15 @@ app.post('/api/login', async(req,res) => {
     res.json({ success: true, role: user.role, userid: user.user_id });
 });
 
-app.get('/api/users/dogs/:ownerId', async (req, res) => {
-    const ownerId = req.params.ownerId;
-    const conn = await pool.getConnection();
-    const [dogs] = await conn.query(
-        'SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [ownerId]
-    );
-    conn.release();
-    res.json(dogs);
-});
+// app.get('/api/users/dogs/:ownerId', async (req, res) => {
+//     const ownerId = req.params.ownerId;
+//     const conn = await pool.getConnection();
+//     const [dogs] = await conn.query(
+//         'SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [ownerId]
+//     );
+//     conn.release();
+//     res.json(dogs);
+// });
 
 //added  end ----------------------------------------------------------------------------------------------------------------
 
