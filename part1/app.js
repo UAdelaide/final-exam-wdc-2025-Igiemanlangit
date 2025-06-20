64fad40586a5ccc26137dd7c1102138fdaf5a3c0
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-20 07:56:41 +0000

    [automatic commit] 2025-06-20 07:56:41 UTC 129.127.202.118

--- part1/app.js
+++ part1/app.js
@@ -65,19 +65,19 @@ async function insertInitialData() {
     }
 }
 
-app.get('/api/dogs', async (req, res) => {
+router.get('/dogs', async (req, res) => {
     try {
-        const [dogs] = await pool.query(`
+      const db = req.app.locals.db;
+      const [rows] = await db.execute(`
         SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
         FROM Dogs
         JOIN Users ON Dogs.owner_id = Users.user_id
       `);
-        res.json(dogs);
+      res.json(rows);
     } catch (err) {
-        console.error(' /api/dogs error:', err);
-        res.status(500).json({ error: 'Failed to fetch dogs' });
+      res.status(500).json({ error: 'Failed to fetch dogs' });
     }
-});
+  });
 
 
 
