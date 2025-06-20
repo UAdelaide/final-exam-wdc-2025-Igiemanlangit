581924253b7342cdbc9b5793af757c2ef3e4a061
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-20 17:32:50 +0000

    [automatic commit] 2025-06-20 17:32:50 UTC 1.121.178.119

--- part1/app.js
+++ part1/app.js
@@ -58,7 +58,7 @@ async function insertInitialData() {
       ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 11:30:00', 10, 'medium Ave', 'completed'),
       ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 12:30:00', 15, 'long Ave', 'cancelled');
     `);
-
+        
         console.log(' Initial data inserted.');
     } catch (err) {
         console.error(' Error inserting data:', err);
