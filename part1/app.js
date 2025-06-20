f4ef744cc27d0fa13d4f1a2ba08259685380da8c
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-20 17:35:15 +0000

    [automatic commit] 2025-06-20 17:35:15 UTC 1.121.178.119

--- part1/app.js
+++ part1/app.js
@@ -69,6 +69,8 @@ async function insertInitialData() {
     }
 }
 
+
+//route to get all dogs and their 
 app.get('/api/dogs', async (req, res) => {
     try {
         const conn = await pool.getConnection();
