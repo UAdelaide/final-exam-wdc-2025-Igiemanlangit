18dd3ec586e1c8d711c3a5725e672f7e9159eac7
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-20 07:56:21 +0000

    [automatic commit] 2025-06-20 07:56:21 UTC 129.127.202.118

--- part1/app.js
+++ part1/app.js
@@ -79,22 +79,6 @@ app.get('/api/dogs', async (req, res) => {
     }
 });
 
-router.get('/walkrequests/open', async (req, res) => {
-    try {
-      const db = req.app.locals.db;
-      const [rows] = await db.execute(`
-        SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time,
-               WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
-        FROM WalkRequests
-        JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
-        JOIN Users ON Dogs.owner_id = Users.user_id
-        WHERE WalkRequests.status = 'open'
-      `);
-      res.json(rows);
-    } catch (err) {
-      res.status(500).json({ error: 'Failed to fetch walkerrequests/open ' });
-    }
-  });
 
 
 app.get('/api/walkrequests/open', async (req, res) => {
