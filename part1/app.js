a8ff0e916bf80110bbc6c7164fa317c7c42e56c8
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-20 07:15:49 +0000

    [automatic commit] 2025-06-20 07:15:49 UTC 129.127.202.118

--- part1/app.js
+++ part1/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mysql = require('mysql2/promise');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require('mysql2/promise');
 
 const app = express();
 
