254740997d1700885f7f94a7c1b7f64de688545c
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-20 14:46:43 +0000

    [automatic commit] 2025-06-20 14:46:43 UTC 1.121.178.119

---
+++ part1/public/browser.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const img = document.getElementById('goodboy');
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+            img.src = data.message;
+            img.style.display = 'block';
+        })
+        .catch(error => console.error('Failed to fetch dog image:', error));
+});
