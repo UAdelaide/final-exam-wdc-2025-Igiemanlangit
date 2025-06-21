0cdd316e88b61613c696295a5def4debc5bc436e
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-21 06:28:34 +0000

    [automatic commit] 2025-06-21 06:28:34 UTC 129.127.202.118

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+const cookieParser = require('cookie-parser'); //q16
 
 //added  start --------------------------------------------------------------------------------------------------------------
 const session = require('express-session'); //q16
