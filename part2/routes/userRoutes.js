cd0eb93e4d3abe21dde3ec4ecb057f044f37bddf
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-21 05:08:36 +0000

    [automatic commit] 2025-06-21 05:08:36 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.118

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -97,6 +97,7 @@ router.post('/logout', (req, res) => {
   });
 });
 
+//q16
 router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
