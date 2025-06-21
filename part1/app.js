45505681b17330ca2b26636148e7976eaea181a1
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-21 07:07:46 +0000

    [automatic commit] 2025-06-21 07:07:46 UTC 129.127.202.118

--- part1/app.js
+++ part1/app.js
@@ -135,8 +135,8 @@ app.get('/api/walkers/summary', async (req, res) => {
 
 insertInitialData();
 
-app.listen(3001, () => {
-    console.log('Server running on http://localhost:3001');
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
 });
 
 module.exports = app;
