1f8e34eb19144c60453ce082689926889418f48c
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-21 03:47:29 +0000

    [automatic commit] 2025-06-21 03:47:29 UTC 129.127.202.118

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -72,13 +72,18 @@ router.get('/me', (req, res) => {
 
 
 //
-router.get('/dogs', async (req, res) => {
-  const ownerID = req.session.user.user_id;
+router.get('/dogs/:ownerId', async (req, res) => {
+  const ownerId = req.params.ownerId;
+
   try {
-    const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [ownerID]);
-    res.json(rows);
+    const [dogs] = await db.query(
+      'SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [ownerId]
+    );
+    res.json(dogs);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch Dogs' });
+    console.error('Failed to fetch dogs:', error);
+    res.status(500).json({ error: 'Database error' });
   }
 });
+
 module.exports = router;
