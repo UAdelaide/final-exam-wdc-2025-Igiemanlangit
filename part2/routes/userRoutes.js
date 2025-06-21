21f3411eca596ab1c7261a9ef047ee937a3a3cf8
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-21 05:39:07 +0000

    [automatic commit] 2025-06-21 05:39:07 UTC 129.127.202.118

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,12 +62,4 @@ router.post('/logout', (req, res) => {
   });
 });
 
-//q16
-router.get('/me', (req, res) => {
-  if (!req.session.user) {
-    return res.status(401).json({ error: 'Not logged in' });
-  }
-  res.json(req.session.user);
-});
-
 module.exports = router;
