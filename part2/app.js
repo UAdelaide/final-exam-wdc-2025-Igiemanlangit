632ef615663bad9c2028ac0c5d6f62ba10dba36b
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-06-21 07:01:37 +0000

    [automatic commit] 2025-06-21 07:01:37 UTC 129.127.202.118

--- part2/app.js
+++ part2/app.js
@@ -13,10 +13,10 @@ app.use(session({ //q16
     resave: false,
     saveUninitialized: false,
     cookie: {
-      httpOnly: true,
-      maxAge: 3600000
+        httpOnly: true,
+        maxAge: 3600000
     }
-  }));
+}));
 //added  end ----------------------------------------------------------------------------------------------------------------
 
 
@@ -39,35 +39,35 @@ app.post('/api/login', async (req, res) => {
     let conn;
 
     try {
-      conn = await pool.getConnection();
+        conn = await pool.getConnection();
 
-      const [users] = await conn.query(
-        'SELECT * FROM Users WHERE username = ? AND password_hash = ?',
-        [username, password]
-      );
+        const [users] = await conn.query(
+            'SELECT * FROM Users WHERE username = ? AND password_hash = ?',
+            [username, password]
+        );
 
-      if (users.length === 0) {
-        return res.json({ success: false, message: "Username or password is incorrect." });
-      }
+        if (users.length === 0) {
+            return res.json({ success: false, message: "Username or password is incorrect." });
+        }
 
-      const user = users[0];
+        const user = users[0];
 
-      req.session.user = {
-        user_id: user.user_id,
-        role: user.role,
-        username: user.username
-      };
+        req.session.user = {
+            user_id: user.user_id,
+            role: user.role,
+            username: user.username
+        };
 
-      res.json({ success: true, role: user.role, user_id: user.user_id });
+        res.json({ success: true, role: user.role, user_id: user.user_id });
 
     } catch (err) {
-      console.error("Login error:", err);
-      res.status(500).json({ success: false, message: "Server error" });
+        console.error("Login error:", err);
+        res.status(500).json({ success: false, message: "Server error" });
 
     } finally {
-      if (conn) conn.release();
+        if (conn) conn.release();
     }
-  });
+});
 
 //added  end ----------------------------------------------------------------------------------------------------------------
 
